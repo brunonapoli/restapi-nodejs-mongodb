3008264ecbd1b61cdc8e3d54c8965c3615671619
Bruno Napoli <napolii.bruno@gmail.com>
2022-01-13 11:47:03 -0300

    Add index.js and server.js

---
+++ src/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+//Settings
+app.set('port', process.env.PORT || 3000);
+
+//Routes
+app.get('/', (req, res) => {
+    res.send('Working')
+});
+
+export default app;
