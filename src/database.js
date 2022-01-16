102f0975fc273d275b5983fe27b69fc35c7fe4fc
Bruno Napoli <napolii.bruno@gmail.com>
2022-01-16 14:41:52 -0300

    DELETE

--- src/database.js
+++ src/database.js
@@ -4,7 +4,6 @@ export async function connect(){
     try {
         const client = await MongoClient.connect('mongodb://localhost:27017');
         const db = client.db('node-mongodb-restapi');
-        console.log('DB is connected');
         return db;
     } catch (error) {
         console.log(error);
--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { connect } from "../database";
+import { ObjectId } from "mongodb";
 
 const router = Router();
 
@@ -15,8 +16,30 @@ router.post('/', async (req, res) => {
         title: req.body.title,
         description: req.body.description
     };
-    const result = await db.collection('tasks').insertOne(task); //insert data into task
+    const result = await db.collection('tasks').insertOne(task); //insert data into task collection
     res.json(result);
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const db = await connect();
+        const resultId = await db.collection('tasks').findOne({_id: ObjectId(id)});
+        res.json(resultId);
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const db = await connect();
+        const deleteId = await db.collection('tasks').findOneAndDelete({_id: ObjectId(id)});
+        res.json(deleteId);
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 export default router;
